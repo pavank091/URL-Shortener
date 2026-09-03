10dd05f2c69718166085403e23c5a6a3930f5476
Pavan Kumar <pavankumar82371@gmail.com>
2026-09-03 16:34:21 +0530

    Adding Analytics

--- controllers/url.js
+++ controllers/url.js
@@ -16,8 +16,18 @@ async function handleGenerateNewShortUrl(req, res) {
     });
 
     return res.status(201).json({ id: shortID});
-}
+};
+
+async function handleGetAnalytics(req, res) {
+    const shortId = req.params.shortId;
+    const result = await URL.findOne({ shortId });
+    return res.json({ 
+        totalClicks: result.visitHistory.length,
+        analytics: result.visitHistory,
+     });
+};
 
 module.exports = {
     handleGenerateNewShortUrl,
-}
+    handleGetAnalytics,
+};
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { connectToMongoDB } = require("./connect");
 const urlRoute = require("./routes/url");
+const URL = require("./models/url");
 
 const app = express();
 const PORT = 8001;
@@ -14,5 +15,26 @@ connectToMongoDB('mongodb://localhost:27017/short-url')
  
 // route
 app.use("/url", urlRoute);
+
+// dynamic route 
+app.get("/:shortId", async (req, res) =>{
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate({
+        shortId
+    }, { $push : {
+            visitHistory: {
+                timestamp: Date.now(),
+            }
+            },
+        }
+    );
+
+    if(!entry){
+        return res.status(404).json({
+            error: "Short url not found"
+        });
+    }
+    res.redirect(entry.redirectURL);
+});
 
 app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`));
